d7fc5331fb1576504f53c94256718efa90c93085
Simon Rosner <simon.rosner.cft@jcrew.com>
2018-10-08 10:25:11 -0400

    Seperated objects and program. Added comments

---
+++ WordWeb.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+class WordNode:
+    """
+    Object representing a word and it's followers
+    """
+    def __init__(self, word):
+        """
+        Initialize with a string
+        """
+        self.word = word
+        self.followers = []
+        #can be used to begin a sentence
+        self.starter = False
+        #can be used to end a sentence
+        self.ender = False
+        #empty/base nodes should be allowed
+        if word != None:
+            if word.istitle():
+                self.starter = True
+            #characters that denote the end of a sentence
+            if word[-1] in ['!','?','.']:
+                self.ender = True  
+        else:
+            self.ender = False
+            
+    def add(self, f):
+        """
+        Add a follower
+        """
+        self.followers.append(f)
+
+class WordWeb:
+    """
+    A collection of WordNodes
+    """
+    def __init__(self):
+        """
+        Initialize with a base node
+        """
+        self.nodes = {None: WordNode(None)}
+        
+    def add(self, front, follow):
+        """
+        Add a new wordNode
+        """
+        if follow not in self.nodes.keys():
+            #create a new WordNode
+            self.nodes[follow] = WordNode(follow)
+        #add it to a previously exising WordNode
+        self.nodes[front].add(self.nodes[follow])     
+
